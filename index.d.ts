78b707c13cfe3197c1244fbe5803206b8a730ed8
Tejesh Mehta <tejesh.mehta@gmail.com>
2020-03-22 11:33:19 -0700

    improve typescript types

--- index.d.ts
+++ index.d.ts
@@ -4,9 +4,9 @@ declare module 'static-observable' {
   export default class StaticObservable<ValueType> extends Observable<
     ValueType
   > {
-    static next(val: ValueType): StaticObservable<ValueType>
-    static error(error: Error): StaticObservable<ValueType>
-    static complete(): StaticObservable<ValueType>
+    static next<ValueType>(val: ValueType): StaticObservable<ValueType>
+    static error<ValueType>(error: Error): StaticObservable<ValueType>
+    static complete<ValueType>(): StaticObservable<ValueType>
     next(val: ValueType): this
     error(error: Error): this
     complete(): this
